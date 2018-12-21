42b9f446d7e189a4c9de88437399c12a81e5d03e
Ryan Agnello <ryanagnello213@gmail.com>
2018-12-20 20:13:40 -0500

    made the number and clear buttons appear in results

---
+++ calculator-javascript.js
@@ -0,0 +1,54 @@
+function wipe() {
+  let clear = "";
+  document.getElementById("result").innerHTML = clear ;
+}
+
+function nine() {
+  let nine = "9";
+  document.getElementById("result").innerHTML += nine ;
+}
+
+function eight() {
+  let eight = "8";
+  document.getElementById("result").innerHTML += eight ;
+}
+
+function seven() {
+  let seven = "7";
+  document.getElementById("result").innerHTML += seven ;
+}
+
+function six() {
+  let six = "6";
+  document.getElementById("result").innerHTML += six ;
+}
+
+function five() {
+  let five = "5";
+  document.getElementById("result").innerHTML += five ;
+}
+
+function four() {
+  let four = "4";
+  document.getElementById("result").innerHTML += four ;
+}
+
+function three() {
+  let three = "3";
+  document.getElementById("result").innerHTML += three ;
+}
+
+function two() {
+  let two = "2";
+  document.getElementById("result").innerHTML += two ;
+}
+
+function one() {
+  let one = "1";
+  document.getElementById("result").innerHTML += one ;
+}
+
+function zero() {
+  let zero = "0";
+  document.getElementById("result").innerHTML += zero;
+}
