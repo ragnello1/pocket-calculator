3302501a0fcd8569da8cc6321ec2bfa9b7e34cf8
Ryan <ryanagnello213@gmail.com>
2019-01-22 23:51:57 -0500

    working

--- calculator-javascript.js
+++ calculator-javascript.js
@@ -1,162 +1,154 @@
 var x = "";
-var number = "";
-var dec = false;
-var y = "";
-var z = "";
-var numbers = [];
+var numb1;
+var numb2;
 var sign;
-function numberPress() {
-  document.getElementById("result").innerHTML = y;
-  console.log(y, x);
+var extra = "0";
+var z;
+var w;
+var dec = false;
+var pressagain = false;
+function equals() {
+  numb2 = x
+  if (sign === "-" && numb2 < 0) {
+    sign = "+";
+    numb2 = -1 * numb2;
+  }
+  let result = eval(numb1 + sign + numb2);
+  w = Number(result);
+  w = parseFloat((w).toFixed(2));
+  if (result > 999999999 || result < 0.00001 && result > 0) {
+      z = w.toExponential();
+    } else {
+      z = w;
+    }
+  document.getElementById("result").innerHTML = z;
+  x = result;
+  console.log(result, x, numb1, numb2);
 }
+
+function wipe() {
+  x = "";
+  numb1 = "";
+  dec = false;
+  w = "";
+  z = "";
+  result = "";
+  sign = "";
+  document.getElementById("result").innerHTML = 0;
+}
+
+function numberPress() {
+  document.getElementById("result").innerHTML = x;
+}
+
+function decimal() {
+  if (dec === false) {
+    x+=".";
+    numberPress();
+    dec = true;}
+}
+
+function plus() {
+  numb1 = x;
+  x = "";
+  sign = "+";
+  document.getElementById("result").innerHTML = sign;
+  console.log(numb1);
+  dec = false;
+}
+
+function minus() {
+  numb1 = x;
+  x = "";
+  sign = "-";
+  document.getElementById("result").innerHTML = sign;
+  console.log(numb1);
+  dec = false;
+}
+
+function mult() {
+  numb1 = x;
+  x = "";
+  sign = "*";
+  document.getElementById("result").innerHTML = sign;
+  console.log(numb1);
+  dec = false;
+}
+
+function divide() {
+  numb1 = x;
+  x = "";
+  sign = "/";
+  document.getElementById("result").innerHTML = sign;
+  console.log(numb1);
+  dec = false;
+}
+
+function neg() {
+  x = Number(x * -1);
+  x = String(x);
+  console.log(x)
+  numberPress();
+}
+
+function perc() {
+  x = (x / 100);
+  x = String(x);
+  console.log(x);
+  numberPress();
+}
+
+
+
+//Numbers
+
 
 function nine() {
   x+="9";
-  y+="9";
   numberPress();
 }
 
 function eight() {
   x+="8";
-  y+="8";
   numberPress();
 }
 
 function seven() {
   x+="7";
-  y+="7";
   numberPress();
 }
 
 function six() {
   x+="6";
-  y+="6";
   numberPress();
 }
 
 function five() {
   x+="5";
-  y+="5";
   numberPress();
 }
 
 function four() {
   x+="4";
-  y+="4";
   numberPress();
 }
 
 function three() {
   x+="3";
-  y+="3";
   numberPress();
 }
 
 function two() {
   x+="2";
-  y+="2";
   numberPress();
 }
 
 function one() {
   x+="1";
-  y+="1";
   numberPress();
 }
 
 function zero() {
   x+="0";
-  y+="0";
   numberPress();
-}
-
-function decimal() {
-  if (dec === false) {
-  x+=".";
-  y+=".";
-  numberPress();
-  dec = true;}
-}
-
-function wipe() {
-  x = "";
-  y = "";
-  dec = false;
-  console.log(y,x);
-  document.getElementById("result").innerHTML = "0";
-}
-
-function plus() {
-  document.getElementById("result").innerHTML = "+";
-  numbers.push(y);
-  x += "+";
-  sign = "plus";
-  dec=false;
-  y = "";
-}
-
-function minus() {
-  document.getElementById("result").innerHTML = "−";
-  numbers.push(y);
-  x += "-";
-  dec=false;
-  sign = "minus";
-  y = "";
-}
-
-function mult() {
-  document.getElementById("result").innerHTML = "×";
-  numbers.push(y);
-  x += "*";
-  dec=false;
-  sign = "multiply";
-  y = "";
-}
-
-function divide() {
-  document.getElementById("result").innerHTML = "÷";
-  numbers.push(y);
-  x += "/";
-  dec=false;
-  sign = "divide";
-  y = "";
-}
-
-function neg() {
-  if (y !== "0") {
-    y = y * -1;
-    x = x * -1;
-    document.getElementById("result").innerHTML = y;
-  }
-}
-
-function perc() {
-
-}
-
-function equals() {
-  numbers.push(y);
-  var numb = eval(x);
-  x = numb;
-  if (x > 999999999 || x < 0.999999999 && x > 0) {
-    x = numb;
-    z = x.toExponential();
-  } else {
-    z = x;
-  }
-  console.log(numb, z, x, y);
-  document.getElementById("result").innerHTML = z;
-  if (sign === "plus") {
-    x += Number(y);
-  }
-  if (sign === "minus") {
-    x -= Number(y);
-  }
-  if (sign === "multiply") {
-    x *= Number(y);
-  }
-  if (sign === "divide") {
-    x /= Number(y);
-  }
-  z="";
 }
